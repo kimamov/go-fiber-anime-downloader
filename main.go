f26aa795ffab2563bd80c6e552cf5677471a04f2
kantimam <kantemir.imam@gmail.com>
2020-08-11 10:21:21 +0200

    progress

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"sort"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/djimenez/iconv-go"
 	"github.com/gofiber/fiber"
 )
 
@@ -58,21 +61,59 @@ func GetVideo() (string, error) {
 	return out, nil
 }
 
-func GetDocument(url string) (*goquery.Document, error) {
-	resp, err := http.Get(url)
+type JSONBody struct {
+	Html string `json:html`
+}
+
+func GetJsonDocument(url string) (*goquery.Document, error) {
+	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
 
-	// Convert HTML into goquery document
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	// parse json from res.body
+	var result map[string]string
+	json.Unmarshal(body, &result)
+	// use utfBody using goquery
+	fmt.Println(result["html"])
+	resultReader := strings.NewReader(result["html"])
+	doc, err := goquery.NewDocumentFromReader(resultReader)
 	if err != nil {
 		return nil, err
 	}
 	return doc, nil
 }
 
-func GetData() (string, error) {
+func GetDocument(url string) (*goquery.Document, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	// Convert the designated charset HTML to utf-8 encoded HTML.
+	// `charset` being one of the charsets known by the iconv package.
+	utfBody, err := iconv.NewReader(res.Body, "UTF-8", "utf-8")
+	if err != nil {
+		return nil, err
+	}
+
+	// use utfBody using goquery
+	doc, err := goquery.NewDocumentFromReader(utfBody)
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
+}
+
+func GetDataA() (string, error) {
 
 	// Get the HMTML
 	animeDocument, err := GetDocument("https://9anime.to/watch/tower-of-god-dub.kvjr/ojo9nqz")
@@ -117,6 +158,39 @@ func GetData() (string, error) {
 	return out, nil
 }
 
+func GetData() (map[int]string, error) {
+
+	// Get the HMTML
+	animeDocument, err := GetDocument("https://9anime.to/watch/tower-of-god-dub.kvjr/ojo9nqz")
+	if err != nil {
+		return nil, err
+	}
+	// get the data-id from the player div
+	dataId, ok := animeDocument.Find("div#player").Attr("data-id")
+	if !ok {
+		return nil, nil
+	}
+	// use data-id to a list of episodes
+	animeStreamsDocument, err := GetJsonDocument(fmt.Sprintf("https://9anime.to/ajax/film/servers?id=%s", dataId))
+	if err != nil {
+		return nil, err
+	}
+
+	// try to get the div containing the episodes hosted on streamtape
+	var episodesMap map[int]string
+	episodesMap = make(map[int]string)
+	container := animeStreamsDocument.Find(`div[data-id="40"]`)
+	container.Find("a").Each(func(i int, s *goquery.Selection) {
+		episodeId, ok := s.Attr("data-id")
+		if ok {
+			episodesMap[i] = episodeId
+		}
+	})
+	//fmt.Println(episodesMap)
+
+	return episodesMap, nil
+}
+
 func main() {
 	app := fiber.New()
 
@@ -147,6 +221,15 @@ func main() {
 			c.Send("failed to get")
 		}
 		c.Send(resp.Body)
+	})
+
+	app.Get("/proxydata", func(c *fiber.Ctx) {
+		resp, err := GetJsonDocument("https://9anime.to/ajax/film/servers?id=kvjr")
+		if err != nil {
+			c.Send("failed to get")
+		}
+		a := resp.Find(`div[data-id="40"]`)
+		c.Send(goquery.OuterHtml(a))
 	})
 
 	app.Listen(3000)
